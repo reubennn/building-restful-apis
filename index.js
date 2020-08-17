7be00f84106a5b2f8e93a7b242fe6c73e5fff608
Reuben Smith <reuben-smith@hotmail.com>
2020-08-17 14:36:27 +0930

    Creates basic routing endpoints
    * Adds basic routing endpoints in src/routs/crmRoutes.js
        - /contact
            - GET, POST
        - /contact/:contactID
            - PUT, DELETE
    * Implements the routes in index.js
        - Imports the routes
        - Passes app into routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import Express from "express"; // ES6
+import express from "express"; // ES6
+import routes from "./src/routes/crmRoutes";
 
-const app = Express();
+const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get("/", (req, res) => {
     res.send(`Node and Express server running on http://localhost:${PORT}.`);
