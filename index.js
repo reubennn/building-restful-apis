5434de4933a628c72edd0080a2acdd4ebbef168f
Reuben Smith <reuben-smith@hotmail.com>
2020-08-17 14:58:19 +0930

    Creates initial database setup
    * Imports mongoose and body-parser
    * Creates mongoose connection with the mongo database
    * Sets up body-parser so it can parse the data for the API

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express"; // ES6
 import routes from "./src/routes/crmRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
+
+// ---------- Mongoose Connection ---------- //
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CRMdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// ---------- Body Parser Connection ---------- //
+/* Allow body-parser to parse the requests that are coming in,
+** and parse it in a way which is readable by the API.*/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
