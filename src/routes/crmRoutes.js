cd2aac223c42624e225e37a81d1206137a953ae9
Reuben Smith <reuben-smith@hotmail.com>
2020-08-17 16:21:10 +0930

    Creates POST and GET endpoints
    * Implements the crmController in src/controllers
        - Imports mongoose
        - Imports the ContactSchema from crmModel
        - Mongoose creates and compiles the Contact model using the ContactSchema
        - Exports two functions
            1. addNewContact: creates a new contact based on the data sent to the API
                - If error: sends back the error
                - No error: sends back the contact which was saved to the database
            2. getContacts: finds all contacts which are in the database
    * Imports the above functions into crmRoutes
        - Replaces /contact endpoints
            - GET calls getContacts
            - POST calls addNewContact

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,3 +1,8 @@
+import { 
+    addNewContact, 
+    getContacts
+} from "../controllers/crmController";
+
 const routes = (app) => {
     app.route("/contact")
         .get((req, res, next) => {
@@ -5,13 +10,9 @@ const routes = (app) => {
             console.log(`Request from: ${req.originalUrl}`);
             console.log(`Request type: ${req.method}`);
             next();
-        }, (req, res, next) => {
-            res.send("GET request successful.");
-        })
+        }, getContacts)
 
-        .post((req, res) => {
-            res.send("POST request successful.");
-        });
+        .post(addNewContact);
 
     app.route("/contact/:contactID")
         .put((req, res) => {
